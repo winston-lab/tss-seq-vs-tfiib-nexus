b4498b92a68a03701f1303225cdb53c4ab16ef88
James Chuang <mrjc@bu.edu>
2017-12-14 15:48:46 -0500

    Add n values to diffexp plot.

--- scripts/tss-v-tfiib-diffexp.R
+++ scripts/tss-v-tfiib-diffexp.R
@@ -16,13 +16,18 @@ df$category = fct_inorder(df$category, ordered=TRUE) %>%
 df = df %>% group_by(chrom, tss_start, tss_end, group, category) %>%
         arrange(desc(tfiib_logpadj)) %>% slice(1) %>% ungroup()
 
-scatter = ggplot(data = df, aes(x=tss_lfc, y=tfiib_lfc)) +
+xmin = min(df$tss_lfc, na.rm=TRUE)
+ymax = max(df$tfiib_lfc, na.rm=TRUE)
+
+countdf = df %>% group_by(group, category) %>% count()
+
+scatter = ggplot() +
             geom_vline(xintercept=0, color="grey85") +
             geom_hline(yintercept=0, color="grey85") +
-            #geom_point(alpha=0.5) +
-            #geom_smooth(method="lm", color="black", size=0.5) +
-            stat_bin_hex(geom="point", aes(color=log10(..count..)),
-                         binwidth=c(0.1,0.1), shape=16, size=0.15, alpha=0.8, stroke=0) +
+            stat_bin_hex(data = df, aes(x=tss_lfc, y=tfiib_lfc, color=log10(..count..)),
+                         geom="point", binwidth=c(0.1,0.1),
+                         shape=16, size=0.15, alpha=0.8, stroke=0) +
+            geom_text(data=countdf, aes(label=paste0("n=",n)), x=.98*xmin,y=.9*ymax, hjust=0, size=4) +
             scale_color_viridis(option="inferno", guide=FALSE) +
             xlab(expression(bold(paste("TSS-seq ", log[2]~frac(condition, control))))) +
             ylab(expression(bold(paste("TFIIB ChIP-nexus ", log[2]~frac(condition, control))))) +
