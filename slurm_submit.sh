690e1e0e6eb9876ff52cec999f6a3dacae526d7d
James Chuang <chuang.ja@gmail.com>
2019-03-21 11:56:16 -0400

    Update helper scripts.

--- slurm_submit.sh
+++ slurm_submit.sh
@@ -6,6 +6,38 @@
 #SBATCH -c 1
 #SBATCH -e snakemake.err
 #SBATCH -o snakemake.log
-#SBATCH -J TSS-seq-snakemake
+#SBATCH -J TSS-vs-TFIIB-snakemake
 
-snakemake -p -R `cat <(snakemake --lc --rerun-incomplete) <(snakemake --li --rerun-incomplete) <(snakemake --lp --rerun-incomplete) | sort -u` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --restart-times 1 --cluster "sbatch -p {cluster.queue} -c {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log} --parsable" --cluster-status "bash slurm_status.sh"
+snakemake -p \
+    -R `cat <(snakemake --lc --rerun-incomplete) \
+            <(snakemake --li --rerun-incomplete) \
+            <(snakemake --lp --rerun-incomplete) | sort -u` \
+    --latency-wait 300 \
+    --rerun-incomplete \
+    --cluster-config $(grep -h tss_workflow config.yaml | \
+                        head -n 1 | \
+                        cut -f1 --complement -d ":" | \
+                        awk '{print $1}' | \
+                        paste -d '' - <(echo cluster.yaml)) \
+    --cluster-config $(grep -h nexus_workflow config.yaml | \
+                        head -n 1 | \
+                        cut -f1 --complement -d ":" | \
+                        awk '{print $1}' | \
+                        paste -d '' - <(echo cluster.yaml)) \
+    --cluster-config $(grep -h tfiib_workflow config.yaml | \
+                        head -n 1 | \
+                        cut -f1 --complement -d ":" | \
+                        awk '{print $1}' | \
+                        paste -d '' - <(echo cluster.yaml)) \
+    --cluster-config $(grep -h annotation_workflow config.yaml | \
+                        head -n 1 | \
+                        cut -f1 --complement -d ":" | \
+                        awk '{print $1}' | \
+                        paste -d '' - <(echo cluster.yaml)) \
+    --cluster-config cluster.yaml \
+    --use-conda \
+    --jobs 9999 \
+    --restart-times 1 \
+    --cluster "sbatch -p {cluster.queue} -c {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log} --parsable" \
+    --cluster-status "bash slurm_status.sh"
+
